24905fe9e42852666736a379872fa2c7f672620d
AMMRISHA <ammrishachowdhury821@gmail.com>
2025-01-20 00:59:36 +0530

    6th commit

--- src/components/FooterDetails.jsx
+++ src/components/FooterDetails.jsx
@@ -1,12 +1,16 @@
 import DesDetails from "./DesDetails";
 import './style.css';
+import NewButton from "./NewButton";
 function FooterDetails({ FooterDetails }) {
   return (
     <>
     <div className="footerdes">
       {FooterDetails.map((details) => (
         <ul key={details.id}>
-        <h6> {details.footColName}</h6>
+        <h6> {details.footColName}
+        {details.footColName === "Solutions" && <NewButton />}
+
+       </h6>
         
         <li>
           <DesDetails footname={details.footColVar}></DesDetails>
---
+++ src/components/NewButton.jsx
@@ -0,0 +1,8 @@
+function NewButton()
+{
+  return <>
+    <button style={{border:"1px solid green", borderRadius:"4px"}}>New</button>
+  </>
+}
+
+export default NewButton;
