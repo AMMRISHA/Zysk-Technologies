b27f3d93ce403bec3ad94fed0a21107457c74fe9
AMMRISHA <ammrishachowdhury821@gmail.com>
2025-01-20 01:05:55 +0530

    6th commit

--- src/components/FooterDetails.jsx
+++ src/components/FooterDetails.jsx
@@ -1,16 +1,12 @@
 import DesDetails from "./DesDetails";
 import './style.css';
-
 function FooterDetails({ FooterDetails }) {
   return (
     <>
     <div className="footerdes">
       {FooterDetails.map((details) => (
         <ul key={details.id}>
-        <h6> {details.footColName}
-       
-
-       </h6>
+        <h6> {details.footColName}</h6>
         
         <li>
           <DesDetails footname={details.footColVar}></DesDetails>
